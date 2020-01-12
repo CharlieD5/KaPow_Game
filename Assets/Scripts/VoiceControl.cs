b6d379b5361053d13e65e2665ffd7f1a131c4a43
Charlie <charlied5@comcast.net>
2020-01-11 23:52:25 -0700

    Initial commit

---
+++ Assets/Scripts/VoiceControl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceControl : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("forward", Forward);
+        actions.Add("boing", Boing);
+        actions.Add("down", Down);
+        actions.Add("back", Back);
+        actions.Add("skirt", Skirt);
+        actions.Add("dash", Dash);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward()
+    {
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Dash()
+    {
+        transform.Translate(5, 0, 0);
+    }
+
+
+    private void Back()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Boing()
+    {
+        transform.Translate(0, 1, 0);
+    }
+
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
+
+    private void Skirt()
+    {
+        transform.Translate(5, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
