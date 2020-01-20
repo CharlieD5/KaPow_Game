fdc85d266a79d2aa4dfca033b38bfb7fba526299
Saaransh Vatsa <u1257247@utah.edu>
2020-01-20 16:28:16 -0700

    added rope and destructible script, destructible object can be tested

---
+++ Assets/Scripts/Level Mechanics/Destructible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject DestroyedVersion;
+    private void OnMouseDown() // Change Function to be called when Player Attacks the Destructible Object
+    {
+        Instantiate(DestroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
